259a970d0f605a4b7f9a8011d70abc6729080d17
fluffy <fluffy@beesbuzz.biz>
2019-04-01 23:23:05 -0700

    Update package versions

--- winrun.cmd
+++ winrun.cmd
@@ -4,5 +4,7 @@ ECHO Starting up Publ...
 
 SET PORT=5000
 SET FLASK_DEBUG=1
-set FLASK_ENV=development
-pipenv run python main.py
+SET FLASK_ENV=development
+SET FLASK_APP=main.py
+pipenv run flask run
+
