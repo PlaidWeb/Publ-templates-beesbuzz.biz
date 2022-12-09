2cb92132c9b0b865e1b4d77d308ae4f813b9fb62
fluffy <fluffy@beesbuzz.biz>
2022-12-09 01:28:17 -0800

    Update dependencies and templates

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 set -e
+cd "$(dirname $0)"
 poetry install
 poetry run flask publ reindex
-FLASK_DEBUG=1 FLASK_ENV=development poetry run flask run
+FLASK_DEBUG=1 FLASK_ENV=development poetry run flask run "$@"
+
